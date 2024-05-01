a48b67bb392e190034246261bf80a390397a6e6c
devhumayun <humayunkabir9408@gmail.com>
2024-05-01 06:58:36 +0600

    refactor

--- app/actions/index.js
+++ app/actions/index.js
@@ -4,17 +4,22 @@ import {
   favouriteToggle,
   userLoginByCredentials,
 } from "@/db/quries";
+import { usersModel } from "@/models/users.model";
+import { dbConnect } from "@/services/database";
 import { redirect } from "next/navigation";
 
 export const userRegister = async (formData) => {
   const userData = Object.fromEntries(formData);
-
+  await dbConnect();
+  await usersModel.find();
   const register = await createUser(userData);
   redirect("/login");
 };
 
 export const userLogin = async (formData) => {
   try {
+    await dbConnect();
+    await usersModel.find();
     const credentails = {};
     credentails.email = formData.get("email");
     credentails.password = formData.get("password");
